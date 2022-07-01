76bb872a2eba1b605849c70cce6c5fb2cbb7d4a2
Vitaliy <bykov.vitalya.93@mail.ru>
2022-07-01 14:06:54 +0300

    new

---
+++ lesson1/Program.cs
@@ -0,0 +1,33 @@
+﻿//! Дз
+//? Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.WriteLine("Введите первое число ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите Второе число ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if(a > b) {
+// 	Console.WriteLine(a + " > " + b);
+// } else {
+// 	Console.WriteLine(a + " < " + b);
+// }
+
+//? Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Console.WriteLine("Введите первое число ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите Второе число ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите Второе число ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+
+// if (a > max){max = a;}
+// if (b > max){max = b;}
+// if (c > max){max = c;}
+
+// Console.Write("максимальное чвисло = " + max);
+
+// Остольное допишу просто времени пока мало)
